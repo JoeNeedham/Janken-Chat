ff04050fe487c1af5623dd796e3425ad715b5524
joeneedum <joeneedum@gmail.com>
2022-11-02 21:03:06 -0400

    create-header

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4a99f66074c7b186cc82";
+/******/ 	var hotCurrentHash = "9273bbaa43f82bd855a0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,21 @@
 import Head from 'next/head'
 import React, { useState } from 'react'
+import { Header } from '../components/Header';
 
 export default function Home() {
   const [advancedMode, setAdvancedMode] = useState(false);
+  const [open, setOpen] = useState(false);
   function changeMode(event: React.MouseEvent){
     event.preventDefault();
     setAdvancedMode(!advancedMode)
   }
   return (
     <div>
+      <Head>
+        <title>Jaken Chat</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header advanced={advancedMode} score={0} />
       <footer> 
         <div onClick={changeMode} className="modes">
             {advancedMode ? "Advanced" : "Normal"} Mode
